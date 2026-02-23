427f9ef2ce668769fc9154705b37653c475e7795
FrancescoCaporale <frenk.caporale@gmail.com>
2026-02-23 16:58:53 +0100

    Fix db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "la_tua_password",
-    database: "movies_db"
+    password: "root",
+    database: "webapp-express"
 });
 
 module.exports = connection;
